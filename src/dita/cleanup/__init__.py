71dd75a47f762d36d4302b4298bc31ef6b857afd
Jaromir Hradilek <jhradilek@gmail.com>
2025-08-27 01:17:58 +0200

    Updated the script version.

--- src/dita/cleanup/__init__.py
+++ src/dita/cleanup/__init__.py
@@ -28,7 +28,7 @@ __author__      = 'Jaromir Hradilek'
 __copyright__   = 'Copyright (C) 2025 Jaromir Hradilek'
 __license__     = 'MIT License'
 __description__ = __doc__
-__version__     = '0.9.0'
+__version__     = '0.9.1'
 
 # Expose general information about the project:
 NAME        = 'dita-cleanup'
